e339c07341d30a1daf5157af1949589ed4bddb95
MahatKC <mkarvat@gmail.com>
2021-07-16 16:37:07 -0300

    created fields

---
+++ organizapet/lib/shared/widgets/dbcontroller.dart
@@ -0,0 +1,32 @@
+class dadosPetiano {
+  final String nome;
+  final String? rg;
+  final String? cpf;
+  final String? ra;
+  final String? telefone;
+  final String? email;
+  final String? dataNascimento;
+  final String? ano;
+  final String? temaICV;
+  final String? orientador;
+  final String? camiseta;
+  final String? github;
+  final String? instagram;
+
+  dadosPetiano(
+    this.nome,
+    this.rg,
+    this.cpf,
+    this.ra,
+    this.telefone,
+    this.email,
+    this.dataNascimento,
+    this.ano,
+    this.temaICV,
+    this.orientador,
+    this.camiseta,
+    this.github,
+    this.instagram);
+
+  
+}
