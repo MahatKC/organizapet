d09c4330d6cc0d5b2fb6b37656105be77edbebe7
MahatKC <mkarvat@gmail.com>
2021-08-03 16:55:59 -0300

    visualizar dados with working back button

--- organizapet/lib/modules/visualizar_dados_petiano/visualizar_dados_petiano.dart
+++ organizapet/lib/modules/visualizar_dados_petiano/visualizar_dados_petiano.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:organizapet/modules/dados_petiano/petianos_controller.dart';
 import 'package:organizapet/modules/dados_petiano/petianos_db_controller.dart';
+import 'package:organizapet/modules/useful_functions/back_to_previous.dart';
 import 'package:organizapet/shared/themes/app_colors.dart';
 import 'package:organizapet/shared/themes/app_images.dart';
 import 'package:organizapet/shared/widgets/app_bar/appBar.dart';
@@ -31,77 +32,82 @@ class _VisualizarDadosPetianoState extends State<VisualizarDadosPetiano> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: AppColors.background,
-        drawer: MenuSanduiche(),
-        appBar: AppBar(
-          title: BarraApp(),
-        ),
-        body: FutureBuilder(
-            future: start,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState != ConnectionState.done) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                final dbController = snapshot.data as dadosPetiano;
-                controller.instantiateAll(dbController);
-                return ListView(children: [
-                  PageTitle(title: "Dados do Petiano"),
-                  BoxInfo(
-                    imagem: AppImages.usuario,
-                    texto: controller.nomeController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.rg,
-                    texto: controller.rgController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.cpf,
-                    texto: controller.cpfController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.unioeste,
-                    texto: controller.raController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.telefone,
-                    texto: controller.telefoneController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.email,
-                    texto: controller.emailController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.dataNascimento,
-                    texto: controller.dataNascimentoController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.ano,
-                    texto: controller.anoController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.icv,
-                    texto: controller.temaICVController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.orientador,
-                    texto: controller.orientadorController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.camiseta,
-                    texto: controller.camisetaController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.github,
-                    texto: controller.githubController.text,
-                  ),
-                  BoxInfo(
-                    imagem: AppImages.insta,
-                    texto: controller.instagramController.text,
-                  ),
-                ]);
-              }
-            }));
+    return WillPopScope(
+      onWillPop: () {
+        return back_to_previous(context);
+      },
+      child: Scaffold(
+          backgroundColor: AppColors.background,
+          drawer: MenuSanduiche(),
+          appBar: AppBar(
+            title: BarraApp(),
+          ),
+          body: FutureBuilder(
+              future: start,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  final dbController = snapshot.data as dadosPetiano;
+                  controller.instantiateAll(dbController);
+                  return ListView(children: [
+                    PageTitle(title: "Dados do Petiano"),
+                    BoxInfo(
+                      imagem: AppImages.usuario,
+                      texto: controller.nomeController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.rg,
+                      texto: controller.rgController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.cpf,
+                      texto: controller.cpfController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.unioeste,
+                      texto: controller.raController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.telefone,
+                      texto: controller.telefoneController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.email,
+                      texto: controller.emailController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.dataNascimento,
+                      texto: controller.dataNascimentoController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.ano,
+                      texto: controller.anoController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.icv,
+                      texto: controller.temaICVController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.orientador,
+                      texto: controller.orientadorController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.camiseta,
+                      texto: controller.camisetaController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.github,
+                      texto: controller.githubController.text,
+                    ),
+                    BoxInfo(
+                      imagem: AppImages.insta,
+                      texto: controller.instagramController.text,
+                    ),
+                  ]);
+                }
+              })),
+    );
   }
 
   Future<dadosPetiano> read_db() async {
