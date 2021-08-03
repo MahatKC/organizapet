b0dba674137dffea7c64bdf4c98462669a2e1671
MahatKC <mkarvat@gmail.com>
2021-08-03 16:18:11 -0300

    controller ready

--- organizapet/lib/modules/visualizar_dados_petiano/visualizar_dados_petiano.dart
+++ organizapet/lib/modules/visualizar_dados_petiano/visualizar_dados_petiano.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:organizapet/modules/dados_petiano/petianos_controller.dart';
+import 'package:organizapet/modules/dados_petiano/petianos_db_controller.dart';
 import 'package:organizapet/shared/themes/app_colors.dart';
 import 'package:organizapet/shared/themes/app_images.dart';
 import 'package:organizapet/shared/widgets/app_bar/appBar.dart';
@@ -7,77 +9,106 @@ import 'package:organizapet/shared/widgets/menu/menuSanduiche.dart';
 import 'package:organizapet/shared/widgets/page_title/page_title.dart';
 
 class VisualizarDadosPetiano extends StatefulWidget {
-  const VisualizarDadosPetiano({Key? key}) : super(key: key);
+  final String nome;
+
+  const VisualizarDadosPetiano({required this.nome, Key? key})
+      : super(key: key);
 
   @override
   _VisualizarDadosPetianoState createState() => _VisualizarDadosPetianoState();
 }
 
 class _VisualizarDadosPetianoState extends State<VisualizarDadosPetiano> {
+  final controller = petianosController();
+  bool access_db = true;
+  late Future<void> start;
+
+  @override
+  void initState() {
+    super.initState();
+    start = read_db();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            backgroundColor: AppColors.background,
-            drawer: MenuSanduiche(),
-            appBar: AppBar(
-              title: BarraApp(),
-            ),
-            body: ListView(children: [
-              PageTitle(title: "Dados do Petiano"),
-              BoxInfo(
-                imagem: AppImages.usuario,
-                texto: "Mateus Edival Rodrigues da Silveira",
-              ),
-              BoxInfo(
-                imagem: AppImages.cpf,
-                texto: "100.111.234-56",
-              ),
-              BoxInfo(
-                imagem: AppImages.rg,
-                texto: "10.111.234-5",
-              ),
-              BoxInfo(
-                imagem: AppImages.unioeste,
-                texto: "45678",
-              ),
-              BoxInfo(
-                imagem: AppImages.telefone,
-                texto: "45 99999-3456",
-              ),
-              BoxInfo(
-                imagem: AppImages.email,
-                texto: "mateusedival@gmail.com",
-              ),
-              BoxInfo(
-                imagem: AppImages.dataNascimento,
-                texto: "23/02/1999",
-              ),
-              BoxInfo(
-                imagem: AppImages.ano,
-                texto: "4°",
-              ),
-              BoxInfo(
-                imagem: AppImages.icv,
-                texto: "AM",
-              ),
-              BoxInfo(
-                imagem: AppImages.orientador,
-                texto: "André Brun",
-              ),
-              BoxInfo(
-                imagem: AppImages.camiseta,
-                texto: "G",
-              ),
-              BoxInfo(
-                imagem: AppImages.github,
-                texto: "mateusedival",
-              ),
-              BoxInfo(
-                imagem: AppImages.insta,
-                texto: "mateusedival",
-              ),
-            ])));
+    return Scaffold(
+        backgroundColor: AppColors.background,
+        drawer: MenuSanduiche(),
+        appBar: AppBar(
+          title: BarraApp(),
+        ),
+        body: FutureBuilder(
+            future: start,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState != ConnectionState.done) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                final dbController = snapshot.data as dadosPetiano;
+                controller.instantiateAll(dbController);
+                return ListView(children: [
+                  PageTitle(title: "Dados do Petiano"),
+                  BoxInfo(
+                    imagem: AppImages.usuario,
+                    texto: controller.nomeController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.rg,
+                    texto: controller.rgController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.cpf,
+                    texto: controller.cpfController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.unioeste,
+                    texto: controller.raController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.telefone,
+                    texto: controller.telefoneController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.email,
+                    texto: controller.emailController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.dataNascimento,
+                    texto: controller.dataNascimentoController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.ano,
+                    texto: controller.anoController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.icv,
+                    texto: controller.temaICVController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.orientador,
+                    texto: controller.orientadorController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.camiseta,
+                    texto: controller.camisetaController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.github,
+                    texto: controller.githubController.text,
+                  ),
+                  BoxInfo(
+                    imagem: AppImages.insta,
+                    texto: controller.instagramController.text,
+                  ),
+                ]);
+              }
+            }));
+  }
+
+  Future<dadosPetiano> read_db() async {
+    var dbController = dadosPetiano(nome: widget.nome);
+    if (access_db == true && widget.nome.isNotEmpty) {
+      await dbController.read();
+    }
+    return dbController;
   }
 }
