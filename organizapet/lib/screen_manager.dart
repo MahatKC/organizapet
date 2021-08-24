7b17347e9a803d5feb2e46eb75e012b13f74013f
MahatKC <mkarvat@gmail.com>
2021-08-24 16:25:00 -0300

    telas no screen manager

--- organizapet/lib/screen_manager.dart
+++ organizapet/lib/screen_manager.dart
@@ -8,6 +8,9 @@ import 'package:organizapet/modules/visualizar_dados_petiano/visualizar_dados_ar
 import 'package:organizapet/modules/visualizar_dados_petiano/visualizar_dados_petiano.dart';
 
 import 'modules/current_user_data/current_user_data.dart';
+import 'modules/editar_projeto/editar_projeto.dart';
+import 'modules/lista_projetos/lista_projetos.dart';
+import 'modules/visualizar_projetos/visualizar_projetos.dart';
 
 class ScreenManager extends StatelessWidget {
   const ScreenManager({Key? key}) : super(key: key);
@@ -30,6 +33,15 @@ class ScreenManager extends StatelessWidget {
             dados: ModalRoute.of(context)!.settings.arguments
                 as EditarPetianoArguments),
         '/login_page': (context) => LoginPage(),
+        '/lista_projeto': (context) => ListaProjetos(
+            user:
+                ModalRoute.of(context)!.settings.arguments as CurrentUserData),
+        '/visualizar_projetos': (context) => VisualizarProjetos(
+            dados: ModalRoute.of(context)!.settings.arguments
+                as VisualizarDadosArguments),
+        '/editar_projeto': (context) => EditarProjeto(
+            dados: ModalRoute.of(context)!.settings.arguments
+                as EditarPetianoArguments),
       },
     );
   }
