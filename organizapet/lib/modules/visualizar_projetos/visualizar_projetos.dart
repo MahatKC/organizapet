9894eea1b2b0dac48a66266a6ce1ebd9191e1460
EnergyFall266 <leonardo.vanzin@hotmail.com>
2021-08-30 18:46:31 -0300

    faltou esse

--- organizapet/lib/modules/visualizar_projetos/visualizar_projetos.dart
+++ organizapet/lib/modules/visualizar_projetos/visualizar_projetos.dart
@@ -76,6 +76,7 @@ class _VisualizarProjetosState extends State<VisualizarProjetos> {
                       Padding(
                         padding: const EdgeInsets.only(bottom: 40),
                         child: IconTitleSubtitleBox(
+                          titulo: "Membros",
                           subtitulo: controller.membrosController.text
                         ),
                       ),
