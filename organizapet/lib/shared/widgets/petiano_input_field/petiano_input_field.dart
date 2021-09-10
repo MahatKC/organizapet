ea547a73f4145b7e256e23b7b78cb7fe4f51eddc
EnergyFall266 <leonardo.vanzin@hotmail.com>
2021-09-10 16:19:45 -0300

    gerente

--- organizapet/lib/shared/widgets/petiano_input_field/petiano_input_field.dart
+++ organizapet/lib/shared/widgets/petiano_input_field/petiano_input_field.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:organizapet/modules/popup_adicionar_pessoa/popup_adicionar_pessoa.dart';
+import 'package:organizapet/shared/themes/app_images.dart';
+import 'package:organizapet/shared/widgets/icon_textfield/icon_textfield.dart';
 
 class PetianoInputField extends StatelessWidget {
   final TextEditingController ctrl;
@@ -30,25 +33,43 @@ class PetianoInputField extends StatelessWidget {
 
     if (hint == "RA") {
       return TextField(
-      controller: ctrl,
-      decoration: InputDecoration(hintText: " " + hint),
-      enabled: enable,
-      //inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-      inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*'))],
-      keyboardType: tipo,
-    );
+        controller: ctrl,
+        decoration: InputDecoration(hintText: " " + hint),
+        enabled: enable,
+        //inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        inputFormatters: [
+          FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*'))
+        ],
+        keyboardType: tipo,
+      );
     }
     if (hint == "Nome") {
       return TextField(
-      controller: ctrl,
-      decoration: InputDecoration(hintText: " " + hint),
-      enabled: enable,
-      //inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-      inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'^[a-zA-Z\u00C0-\u00FF\s]*'))],
-      keyboardType: tipo,
-    );
+        controller: ctrl,
+        decoration: InputDecoration(hintText: " " + hint),
+        enabled: enable,
+        //inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        inputFormatters: [
+          FilteringTextInputFormatter.allow(
+              RegExp(r'^[a-zA-Z\u00C0-\u00FF\s]*'))
+        ],
+        keyboardType: tipo,
+      );
     }
-
+    if (hint == "Gerente") {
+      return TextField(
+        decoration: InputDecoration(
+          suffixIcon: IconButton(
+              onPressed: () {
+                showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) => PopupAdicionarPessoa());
+              },
+              icon: Image.asset(AppImages.adicionarMembro)),
+          hintText: "Gerente",
+        ),
+      );
+    }
     return TextField(
       controller: ctrl,
       decoration: InputDecoration(hintText: " " + hint),
