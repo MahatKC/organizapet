ee29abfaa207adf8bb1cf1b9791e29af7f020552
MahatKC <mkarvat@gmail.com>
2021-09-17 15:57:10 -0300

    nome abreviado bug

--- organizapet/lib/modules/useful_functions/nome_abreviado.dart
+++ organizapet/lib/modules/useful_functions/nome_abreviado.dart
@@ -3,6 +3,13 @@ import 'package:organizapet/modules/useful_functions/first_and_last_name.dart';
 String nome_abreviado(String nome, String nomeCurto) {
   String nome_curto = first_and_last_name_from_string(nome, nomeCurto);
   List name_list = nome_curto.split(" ");
-  String nome_abreviado = name_list[0] + " " + name_list[1][0];
+
+  String nome_abreviado;
+  if (name_list.length <= 1) {
+    nome_abreviado = name_list[0];
+  }else{
+    nome_abreviado = name_list[0] + " " + name_list[1][0];
+  }
+  
   return nome_abreviado;
 }
